e278bb4b18c1102b732ecef6e3a888c20bb4e370
TPocmHuK <vechtomov1@yandex.ru>
2021-03-27 13:15:13 +0400

    hello world

---
+++ Assets/Scripts/CreateCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    public GameObject[] Cubes;
+
+    void Start() 
+    {
+        int cube = Random.Range (0, 4);
+        Instantiate(Cubes[cube], transform.position, Quaternion.identity);
+    }
+    
+}
